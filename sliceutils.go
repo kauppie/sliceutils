8f258223c6d12826395c7aff874e6c8d76fbc884
Elias Kauppi <elkku.kauppi@gmail.com>
2022-05-20 00:30:19 +0300

    Initial commit

---
+++ sliceutils.go
@@ -0,0 +1,168 @@
+package sliceutils
+
+// Count the number of matching items in a slice. Counter is incremented if
+// counter function returns true on them.
+//
+// Panics on nil counter function.
+func Count[T any](slice []T, counterFn func(T) bool) int {
+	count := 0
+	for _, val := range slice {
+		if counterFn(val) {
+			count++
+		}
+	}
+	return count
+}
+
+// Filter values in a slice by filter function. Resulting slice will contain
+// values for which the filter function returns true.
+//
+// Returns nil on nil slice. Panics on nil filter function.
+func Filter[T any](slice []T, filterFn func(T) bool) []T {
+	// Preserve nil.
+	if slice == nil {
+		return nil
+	}
+	outSlice := make([]T, 0)
+	for _, val := range slice {
+		if filterFn(val) {
+			outSlice = append(outSlice, val)
+		}
+	}
+	return outSlice
+}
+
+// Filter and map slice values with filter map function. Resulting slice
+// will contain mapped values for which the filter map function returns true as
+// the second argument. FilterMap is usually more efficient than using Filter
+// and Map separately.
+//
+// Returns nil on nil slice. Panics on nil filter map function.
+func FilterMap[T, U any](slice []T, filterMapFn func(T) (U, bool)) []U {
+	// Preserve nil.
+	if slice == nil {
+		return nil
+	}
+	outSlice := make([]U, 0)
+	for _, val := range slice {
+		if mapped, ok := filterMapFn(val); ok {
+			outSlice = append(outSlice, mapped)
+		}
+	}
+	return outSlice
+}
+
+// Flatten a slice of slices into a slice. Resulting slice preserves order
+// from the original slice where the first values will be from the first slice.
+//
+// Returns nil on nil slice.
+func Flatten[T any](slice [][]T) []T {
+	// Preserve nil.
+	if slice == nil {
+		return nil
+	}
+	outSlice := make([]T, 0)
+	for _, val := range slice {
+		outSlice = append(outSlice, val...)
+	}
+	return outSlice
+}
+
+// Returns the frequency of values in a slice. Resulting map contains the found
+// values as keys and their number of occurrences as values.
+//
+// Returns nil on nil slice.
+func Frequency[T comparable](slice []T) map[T]int {
+	// Preserve nil.
+	if slice == nil {
+		return nil
+	}
+	outMap := make(map[T]int)
+	for _, val := range slice {
+		if _, exists := outMap[val]; exists {
+			outMap[val]++
+		} else {
+			outMap[val] = 1
+		}
+	}
+	return outMap
+}
+
+// Returns true if the slice is sorted by given comparison function. For
+// ascending order, pass a comparison function which returns true when left is
+// less than right.
+func IsSortedBy[T any](slice []T, compFn func(T, T) bool) bool {
+	for i := 1; i < len(slice); i++ {
+		if compFn(slice[i], slice[i-1]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Maps each slice value with mapping function.
+//
+// Returns nil on nil slice. Panics on nil map function.
+func Map[T, U any](slice []T, mapFn func(T) U) []U {
+	// Preserve nil.
+	if slice == nil {
+		return nil
+	}
+	// Reserve capacity eagerly to allocate only once.
+	outSlice := make([]U, 0, len(slice))
+	for _, val := range slice {
+		outSlice = append(outSlice, mapFn(val))
+	}
+	return outSlice
+}
+
+// Partition single slice into two slices using partition function. The first
+// returned slice contains values for which the partition function returns true,
+// and the second slice values for which the function returns false.
+//
+// Returns nil slices on nil slice. Panics on nil map function.
+func Partition[T any](slice []T, partFn func(T) bool) ([]T, []T) {
+	// Preserve nil.
+	if slice == nil {
+		return nil, nil
+	}
+	trueSlice := make([]T, 0)
+	falseSlice := make([]T, 0)
+	for _, val := range slice {
+		if partFn(val) {
+			trueSlice = append(trueSlice, val)
+		} else {
+			falseSlice = append(falseSlice, val)
+		}
+	}
+	return trueSlice, falseSlice
+}
+
+// Reduces a slice successively into a single value. init is the initial value
+// for which the reducer function is applied. Reducer function takes the current
+// reduced value and the next slice value and returns the reduced value.
+//
+// Return initial value for nil slices. Panics on nil reducer function.
+func Reduce[T, U any](slice []T, init U, reducerFn func(U, T) U) U {
+	for _, val := range slice {
+		init = reducerFn(init, val)
+	}
+	return init
+}
+
+// Reverses the order of elements in a slice.
+//
+// Returns nil on nil slice.
+func Reverse[T any](slice []T) []T {
+	// Preserve nil.
+	if slice == nil {
+		return nil
+	}
+	i := len(slice) - 1
+	outSlice := make([]T, len(slice))
+	for _, val := range slice {
+		outSlice[i] = val
+		i--
+	}
+	return outSlice
+}
---
+++ sliceutils_test.go
@@ -0,0 +1,196 @@
+package sliceutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCount(t *testing.T) {
+	t.Run("Count zeros", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 0, 1, 4, 0, 0, 12, 3, 5, 7, 1}
+		count := Count(slice, func(i int) bool { return i == 0 })
+		assert.Equal(t, 3, count)
+	})
+
+	t.Run("Return zero on nil slice", func(t *testing.T) {
+		var slice []int = nil
+		count := Count(slice, func(i int) bool { return i == 0 })
+		assert.Equal(t, 0, count)
+	})
+}
+
+func TestFilter(t *testing.T) {
+	t.Run("Retain strings shorter than 4 characters", func(t *testing.T) {
+		slice := []string{"hello", "foo", "bar", "pointer", "cow", "F"}
+		filtered := Filter(slice, func(s string) bool { return len(s) < 4 })
+		assert.Equal(t, []string{"foo", "bar", "cow", "F"}, filtered)
+	})
+
+	t.Run("Return nil on nil slice", func(t *testing.T) {
+		var slice []int = nil
+		flat := Filter(slice, func(i int) bool { return i < 4 })
+		assert.Nil(t, flat)
+	})
+}
+
+func TestFilterMap(t *testing.T) {
+	ToPointer := func(s string) *string {
+		return &s
+	}
+
+	t.Run("Pointers to concrete types", func(t *testing.T) {
+		slice := []*string{
+			ToPointer("hello"),
+			nil,
+			ToPointer("foo"),
+			nil,
+			nil,
+			ToPointer("bar"),
+		}
+		filterMapped := FilterMap(slice, func(s *string) (string, bool) {
+			if s == nil {
+				return "", false
+			}
+			return *s, true
+		})
+		assert.Equal(t, []string{"hello", "foo", "bar"}, filterMapped)
+	})
+
+	t.Run("Strings to their byte lengths if under 4", func(t *testing.T) {
+		slice := []string{"hello", "foo", "bar", "pointer", "cow", "F"}
+		filterMapped := FilterMap(slice, func(s string) (int, bool) {
+			slen := len(s)
+			return slen, slen < 4
+		})
+		assert.Equal(t, []int{3, 3, 3, 1}, filterMapped)
+	})
+}
+
+func TestFlatten(t *testing.T) {
+	t.Run("Flatten integer slice", func(t *testing.T) {
+		slice := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8}}
+		flat := Flatten(slice)
+		assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8}, flat)
+	})
+
+	t.Run("Return nil on nil slice", func(t *testing.T) {
+		var slice [][]int = nil
+		flat := Flatten(slice)
+		assert.Nil(t, flat)
+	})
+}
+
+func TestFrequency(t *testing.T) {
+	t.Run("Count integer frequency", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 0, 1, 4, 0, 0, 12, 3, 5, 7, 1}
+		frequencies := Frequency(slice)
+		assert.Equal(t, map[int]int{
+			1:  3,
+			2:  1,
+			3:  2,
+			4:  2,
+			0:  3,
+			12: 1,
+			5:  1,
+			7:  1},
+			frequencies,
+		)
+	})
+
+	t.Run("Empty map on empty slice", func(t *testing.T) {
+		slice := []int{}
+		frequencies := Frequency(slice)
+		assert.Equal(t, map[int]int{}, frequencies)
+	})
+}
+
+func TestIsSortedBy(t *testing.T) {
+	t.Run("Is sorted by with sorted slices", func(t *testing.T) {
+		sortedSlice := []int{1, 2, 3, 4, 4, 5, 6, 7, 8}
+		sorted := IsSortedBy(sortedSlice, func(lhs, rhs int) bool {
+			return lhs < rhs
+		})
+		assert.True(t, sorted)
+
+		sortedSlice2 := []string{"bar", "baz", "foo", "hello", "world"}
+		sorted2 := IsSortedBy(sortedSlice2, func(lhs, rhs string) bool { return lhs < rhs })
+		assert.True(t, sorted2)
+
+		reverseSortedSlice := Reverse(sortedSlice)
+		reverseSorted := IsSortedBy(reverseSortedSlice, func(lhs, rhs int) bool { return lhs > rhs })
+		assert.True(t, reverseSorted)
+	})
+
+	t.Run("Is sorted by with unsorted slices", func(t *testing.T) {
+		unsortedSlice := []int{1, 2, 3, 4, 5, 4, 6, 7, 8}
+		sorted := IsSortedBy(unsortedSlice, func(lhs, rhs int) bool {
+			return lhs < rhs
+		})
+		assert.False(t, sorted)
+
+		unsortedSlice2 := []string{"baz", "bar", "foo", "hello", "world"}
+		sorted2 := IsSortedBy(unsortedSlice2, func(lhs, rhs string) bool { return lhs < rhs })
+		assert.False(t, sorted2)
+	})
+}
+
+func TestMap(t *testing.T) {
+	t.Run("Strings to their rune lengths", func(t *testing.T) {
+		slice := []string{"bar", "", "f", "hello", "world"}
+		lengths := Map(slice, func(s string) int { return len(s) })
+		assert.Equal(t, []int{3, 0, 1, 5, 5}, lengths)
+	})
+
+	t.Run("Return nil on nil slice", func(t *testing.T) {
+		var slice []string = nil
+		outSlice := Map(slice, func(s string) int { return len(s) })
+		assert.Nil(t, outSlice)
+	})
+}
+
+func TestPartition(t *testing.T) {
+	t.Run("Partition by integer parity", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		even, odd := Partition(slice, func(i int) bool { return i%2 == 0 })
+		assert.Equal(t, []int{2, 4, 6, 8, 10}, even)
+		assert.Equal(t, []int{1, 3, 5, 7, 9}, odd)
+	})
+
+	t.Run("Return nil on nil slice", func(t *testing.T) {
+		var slice []int = nil
+		even, odd := Partition(slice, func(i int) bool { return i%2 == 0 })
+		assert.Nil(t, even)
+		assert.Nil(t, odd)
+	})
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("Calculate sum and factorial", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6}
+
+		sum := Reduce(numbers, 0, func(acc, next int) int {
+			return acc + next
+		})
+		assert.Equal(t, 1+2+3+4+5+6, sum)
+
+		factorial := Reduce(numbers, 1, func(acc, next int) int {
+			return acc * next
+		})
+		assert.Equal(t, 1*2*3*4*5*6, factorial)
+	})
+}
+
+func TestReverse(t *testing.T) {
+	t.Run("Reverse integer slice", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 5}
+		reversed := Reverse(slice)
+		assert.Equal(t, []int{5, 4, 3, 2, 1}, reversed)
+	})
+
+	t.Run("Return nil on nil slice", func(t *testing.T) {
+		var slice []int = nil
+		output := Reverse(slice)
+		assert.Nil(t, output)
+	})
+}
